6852aee9f647fb33c10b371fbc3188082dc10ccd
Hanieh Gharaee <gharaie.hanieh@gmail.com>
2024-10-27 15:37:06 +0330

    remove sidebar

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
   return (
-    <div className="app-layout">
-      <main className="main-content flex-1 !min-h-[80vh]">
+    <div className="app-layout bg-black">
+      <main className="main-content flex-1 !min-h-[80vh] bg-black">
         <Outlet />
       </main>
     </div>
