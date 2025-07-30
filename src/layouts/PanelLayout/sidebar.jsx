2f3b5758c4e341e91f04ad6b0aa19ab9afabe9cf
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-07-30 16:17:05 +0330

    Put icon for dashboard in sidebar

--- src/layouts/PanelLayout/sidebar.jsx
+++ src/layouts/PanelLayout/sidebar.jsx
@@ -4,7 +4,7 @@ import {
   VideoCameraOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
-import { pharmacyIcon } from "@/assets/icons/index";
+import { pharmacyIcon, homeIcon } from "@/assets/icons/index";
 
 export const Sidebar = ({ collapsed }) => {
   return (
@@ -41,8 +41,17 @@ export const Sidebar = ({ collapsed }) => {
         items={[
           {
             key: "1",
-            icon: <UserOutlined />,
-            label: "nav 1",
+            icon: (
+              <img
+                src={homeIcon}
+                alt="Home Icon"
+                style={{
+                  width: "20px",
+                  filter: "brightness(0) saturate(100%) invert(1)",
+                }}
+              />
+            ),
+            label: "Dashboard",
           },
           {
             key: "2",
@@ -55,24 +64,24 @@ export const Sidebar = ({ collapsed }) => {
             label: "nav 3",
           },
           {
-            key: "3",
-            icon: <UploadOutlined />,
-            label: "nav 3",
+            key: "4",
+            icon: <UserOutlined />,
+            label: "nav 4",
           },
           {
-            key: "3",
+            key: "5",
             icon: <UploadOutlined />,
-            label: "nav 3",
+            label: "nav 5",
           },
           {
-            key: "3",
+            key: "6",
             icon: <UploadOutlined />,
-            label: "nav 3",
+            label: "nav 6",
           },
           {
-            key: "3",
+            key: "7",
             icon: <UploadOutlined />,
-            label: "nav 3",
+            label: "nav 7",
           },
         ]}
       />
