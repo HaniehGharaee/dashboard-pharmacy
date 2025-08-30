8a9776dcc9d9724805b9f714bef007d4c6cd26a1
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-08-30 12:44:36 +0330

    call datas cards dashboard from backend

--- src/components/dashboard/card.jsx
+++ src/components/dashboard/card.jsx
@@ -1,125 +1,123 @@
 import { Card, Col, Row } from "antd";
-import { TableOutlined } from "@ant-design/icons";
 import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import StoreIcon from "@mui/icons-material/Store";
-import MedicationIcon from '@mui/icons-material/Medication';
-import GroupsIcon from '@mui/icons-material/Groups';
+import MedicationIcon from "@mui/icons-material/Medication";
+import GroupsIcon from "@mui/icons-material/Groups";
 
-const stats = [
-  {
-    title: "Total Store",
-    value: "261",
-    icon: <StoreIcon />,
-    color: "#4caf50",
-  },
-  {
-    title: "Total Medicines",
-    value: "530",
-    icon: <MedicationIcon />,
-    color: "#0067ff",
-  },
-  {
-    title: "Total Customers",
-    value: "530",
-    icon: <GroupsIcon />,
-    color: "#ffea00",
-  },
-  {
-    title: "Total Suppliers",
-    value: "118",
-    icon: <GroupsIcon />,
-    color: "#e91e63",
-  },
-  {
-    title: "Total Product Sale",
-    value: "261",
-    icon: <MonetizationOnOutlinedIcon />,
-    color: "#2196f3",
-  },
-  {
-    title: "Today Product Sale",
-    value: "12",
-    icon: <CalendarMonthIcon />,
-    color: "#4caf50",
-  },
-  {
-    title: "This Month Sale",
-    value: "530",
-    icon: <InventoryIcon />,
-    color: "#ff9800",
-  },
-  {
-    title: "This Year Product Sale",
-    value: "118",
-    icon: <PeopleAltIcon />,
-    color: "#e91e63",
-  },
-  {
-    title: "Total Product Purchase",
-    value: "261",
-    icon: <MonetizationOnOutlinedIcon />,
-    color: "#2196f3",
-  },
-  {
-    title: "Today Product Purchase",
-    value: "12",
-    icon: <CalendarMonthIcon />,
-    color: "#4caf50",
-  },
-  {
-    title: "This Month Purchase",
-    value: "530",
-    icon: <InventoryIcon />,
-    color: "#ff9800",
-  },
-  {
-    title: "This Year Product Purchase",
-    value: "118",
-    icon: <PeopleAltIcon />,
-    color: "#e91e63",
-  },
-  {
-    title: "Total Earning",
-    value: "261",
-    icon: <MonetizationOnOutlinedIcon />,
-    color: "#2196f3",
-  },
-  {
-    title: "Today Earning",
-    value: "12",
-    icon: <CalendarMonthIcon />,
-    color: "#4caf50",
-  },
-  {
-    title: "This Month Earning",
-    value: "530",
-    icon: <InventoryIcon />,
-    color: "#ff9800",
-  },
-  {
-    title: "This Year Erning",
-    value: "118",
-    icon: <PeopleAltIcon />,
-    color: "#e91e63",
-  },
-  {
-    title: "Total Categories",
-    value: "118",
-    icon: <PeopleAltIcon />,
-    color: "#e91e63",
-  },
-  {
-    title: "Total Transactions",
-    value: "118",
-    icon: <PeopleAltIcon />,
-    color: "#e91e63",
-  },
-];
-
-const CardsDashboard = () => {
+const CardsDashboard = ({ data }) => {
+  const stats = [
+    {
+      title: "Total Store",
+      value: data?.totalStores ?? 0,
+      icon: <StoreIcon />,
+      color: "#4caf50",
+    },
+    {
+      title: "Total Medicines",
+      value: data?.totalMedicines ?? 0,
+      icon: <MedicationIcon />,
+      color: "#0067ff",
+    },
+    {
+      title: "Total Customers",
+      value: data?.totalCustomers ?? 0,
+      icon: <GroupsIcon />,
+      color: "#ffea00",
+    },
+    {
+      title: "Total Suppliers",
+      value: data?.totalSuppliers ?? 0,
+      icon: <GroupsIcon />,
+      color: "#e91e63",
+    },
+    {
+      title: "Total Product Sale",
+      value: data?.totalProductSale ?? 0,
+      icon: <MonetizationOnOutlinedIcon />,
+      color: "#2196f3",
+    },
+    {
+      title: "Today Product Sale",
+      value: data?.todayProductSale ?? 0,
+      icon: <CalendarMonthIcon />,
+      color: "#4caf50",
+    },
+    {
+      title: "This Month Sale",
+      value: data?.thisMonthSale ?? 0,
+      icon: <InventoryIcon />,
+      color: "#ff9800",
+    },
+    {
+      title: "This Year Product Sale",
+      value: data?.thisYearProductSale ?? 0,
+      icon: <PeopleAltIcon />,
+      color: "#e91e63",
+    },
+    {
+      title: "Total Product Purchase",
+      value: data?.totalProductPurchase ?? 0,
+      icon: <MonetizationOnOutlinedIcon />,
+      color: "#2196f3",
+    },
+    {
+      title: "Today Product Purchase",
+      value: data?.todayProductPurchase ?? 0,
+      icon: <CalendarMonthIcon />,
+      color: "#4caf50",
+    },
+    {
+      title: "This Month Purchase",
+      value: data?.thisMonthPurchase ?? 0,
+      icon: <InventoryIcon />,
+      color: "#ff9800",
+    },
+    {
+      title: "This Year Product Purchase",
+      value: data?.thisYearProductPurchase ?? 0,
+      icon: <PeopleAltIcon />,
+      color: "#e91e63",
+    },
+    {
+      title: "Total Earning",
+      value: data?.totalEarning ?? 0,
+      icon: <MonetizationOnOutlinedIcon />,
+      color: "#2196f3",
+    },
+    {
+      title: "Today Earning",
+      value: data?.todayEarning ?? 0,
+      icon: <CalendarMonthIcon />,
+      color: "#4caf50",
+    },
+    {
+      title: "This Month Earning",
+      value: data?.thisMonthEarning ?? 0,
+      icon: <InventoryIcon />,
+      color: "#ff9800",
+    },
+    {
+      title: "This Year Erning",
+      value: data?.thisYearEarning ?? 0,
+      icon: <PeopleAltIcon />,
+      color: "#e91e63",
+    },
+    {
+      title: "Total Categories",
+      value: data?.totalCategories ?? 0,
+      icon: <PeopleAltIcon />,
+      color: "#e91e63",
+    },
+    {
+      title: "Total Transactions",
+      value: data?.totalTransactions ?? 0,
+      icon: <PeopleAltIcon />,
+      color: "#e91e63",
+    },
+  ];
   return (
     <div className="max-w-[1300px] mx-auto px-4">
       <Row gutter={[12, 12]}>
