bc7c36f95bbe647ad9d2f7d9a780e3ac62e2b5e2
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-09-24 23:28:51 +0330

    Set Form for add medicine

--- src/components/medicine/medicineAdd.jsx
+++ src/components/medicine/medicineAdd.jsx
@@ -2,6 +2,13 @@ import { Button, Input, Form } from "antd";
 import { PlusCircleOutlined } from "@ant-design/icons";
 
 const AddMedicines = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  }
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
     <div className="min-h-screen">
       <div
@@ -19,44 +26,115 @@ const AddMedicines = () => {
             Back
           </Button>
         </div>
-        <div className="flex flex-col gap-2 mt-7">
+        <div className="flex flex-col gap-3 mt-7">
           <Form
             name="basic"
-            initialValues={{
-              remember: true,
-            }}
-            //onFinish={onFinish}
-            //onFinishFailed={onFinishFailed}
+            //labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            // initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Form.Item name="patientName" className="mb-4">
-              <Input
-                //disabled={disabled}
-                label="patientName"
-                span="24"
-                placeholder="patientName"
-                type="text"
-                theme="dark"
-              />
+            <Form.Item
+              label="Barcode"
+              name="Barcode"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
             </Form.Item>
-            <Form.Item name="patientName" className="mb-4">
-              <Input
-                //disabled={disabled}
-                label="patientName"
-                span="24"
-                placeholder="patientName"
-                type="text"
-                theme="dark"
-              />
+            <Form.Item
+              label="Name"
+              name="Name"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
             </Form.Item>
-            <Form.Item name="patientName" className="mb-4">
-              <Input
-                //disabled={disabled}
-                label="patientName"
-                span="24"
-                placeholder="patientName"
-                type="text"
-                theme="dark"
-              />
+            <Form.Item
+              label="Generic Name"
+              name="Generic Name"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Category"
+              name="Category"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Medicine Type"
+              name="Medicine Type"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Unit"
+              name="Unit"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Box Size"
+              name="Box Size"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Medicine Details"
+              name="Medicine Details"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Image"
+              name="Image"
+            //rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Supplier"
+              name="Supplier"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Supplier Price"
+              name="Supplier Price"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Price"
+              name="Price"
+              rules={[{ required: true, message: '' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Vat"
+              name="Vat"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Status"
+              name="Status"
+            >
+            </Form.Item>
+            <Form.Item label={null}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
             </Form.Item>
           </Form>
         </div>
